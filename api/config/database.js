01f4c3cda46ea130b50213ce86a20b434572073f
Priyansh Sharma <priyanshsharma274@gmail.com>
2023-11-17 01:29:27 +0530

    Fixing db bug 2

--- api/config/database.js
+++ api/config/database.js
@@ -5,7 +5,10 @@ const dbConnection = async () => {
   try {
     const mongoURI = process.env.MONGO_URI;
     console.log("Attempting to connect to MongoDB with URI:", mongoURI);
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB Connected Successfully");
   } catch (error) {
     console.log(error);
