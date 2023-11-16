f67631345e3f8e70edd35a1370db745e441e8caf
Priyansh Sharma <priyanshsharma274@gmail.com>
2023-11-17 01:18:08 +0530

    Fixing db bug

--- api/config/database.js
+++ api/config/database.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.Mongo_URI);
+    const mongoURI = process.env.MONGO_URI;
+    console.log("Attempting to connect to MongoDB with URI:", mongoURI);
+    await mongoose.connect(mongoURI);
     console.log("DB Connected Successfully");
   } catch (error) {
     console.log(error);
